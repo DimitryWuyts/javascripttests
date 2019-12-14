0a2fa549c8641fcceb6a4aec79269562ef8d5163
DimitryWuyts <dimiw1337@hotmail.com>
2019-12-14 14:08:08 +0100

    a little reminder file for basic javascript

---
+++ help.js
@@ -0,0 +1,30 @@
+// Primitives / Value types
+
+let name = 'mosh'; //string literal
+let age = 30; //number literal
+let isApproved =true; // boolean literal
+let firstName = undefined; // undefined literal
+let lastName = null; // null literal
+
+//--------------------------------------------------//
+
+// Reference Types
+
+//Object
+let person = {
+    name: 'mosh',
+    age: 30};
+
+// Arrays
+let selectedColors = ['red', 'blue'];
+
+// Function
+function greet(name) {
+    console.log('hello ' + name);
+}
+greet('jhon');
+
+//--------------------------------------------------//
+
+
+
